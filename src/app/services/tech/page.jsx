687f28c930e513a467704a32dc2f98253808dabc
Mounieshh <iammounieshv@gmail.com>
2024-03-28 01:54:28 +0530

    WebTechnologies Added

--- src/app/services/tech/page.jsx
+++ src/app/services/tech/page.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
 
-const TechPage = () => {
+const WebTechPage = () => {
   return (
-    <div>Website Page</div>
-  )
-}
+    <>
+      <section>
+        <div className="slicer">
+          <div className="slice">
+            <h4> A Dream come true moment for your brand </h4>
+            <p> Let your website be a reflection of your brand's unmatched speciality. </p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <h3 className="heading">What We Offer</h3>
+          <div className="box-container">
+            <div className="box">
+              <h3>Web Development</h3>
+              <p>We go beyond coding to craft an immersive digital experience. Our web development services seamlessly blend technology and creativity, ensuring your online presence is not just functional but an engaging journey.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="gradient">
+        <img className="image" src="/photo1.jpg" alt="Web Image" />
+        <div className="overlay">
+          <h2>Join us to build your website</h2>
+          <a href="contact.html" className="bn">Let's connect</a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default TechPage
+export default WebTechPage;
