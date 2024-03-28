f4a4da81e71b93e36110aebec6bf341b84819d45
RamanKishore2004 <ramankishoreramu@gmail.com>
2024-03-28 10:41:12 +0530

    Tech Page modified with module css

--- src/app/services/tech/page.jsx
+++ src/app/services/tech/page.jsx
@@ -1,31 +1,32 @@
+import styles from './webTech.module.css';
 
 const WebTechPage = () => {
   return (
     <>
       <section>
-        <div className="slicer">
-          <div className="slice">
+        <div className={styles.slicer}>
+          <div className={styles.slice}>
             <h4> A Dream come true moment for your brand </h4>
             <p> Let your website be a reflection of your brand's unmatched speciality. </p>
           </div>
         </div>
       </section>
       <section>
-        <div className="container">
-          <h3 className="heading">What We Offer</h3>
-          <div className="box-container">
-            <div className="box">
+        <div className={styles.container}>
+          <h3 className={styles.heading}>What We Offer</h3>
+          <div className={styles.boxContainer}>
+            <div className={styles.box}>
               <h3>Web Development</h3>
               <p>We go beyond coding to craft an immersive digital experience. Our web development services seamlessly blend technology and creativity, ensuring your online presence is not just functional but an engaging journey.</p>
             </div>
           </div>
         </div>
       </section>
-      <div className="gradient">
-        <img className="image" src="/photo1.jpg" alt="Web Image" />
-        <div className="overlay">
+      <div className={styles.gradient}>
+        <img className={styles.image} src="/photo1.jpg" alt="Web Image" />
+        <div className={styles.overlay}>
           <h2>Join us to build your website</h2>
-          <a href="contact.html" className="bn">Let's connect</a>
+          <a href="contact.html" className={styles.bn}>Let's connect</a>
         </div>
       </div>
     </>
